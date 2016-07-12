Homerlv/edk2

commit e7fc78fdd61fe930c1ac775ba8c89e6c3ff871c5

MdeModulePkg: Update PXE driver to follow edk2 coding standards.
Contributed-under: TianoCore Contribution Agreement 1.0
Signed-off-by: Fu Siyuan <siyuan.fu@intel.com>
Reviewed-by: Samer El-Haj-Mahmoud <smahmoud@lenovo.com>
Reviewed-by: Wu Jiaxin <jiaxin.wu@intel.com>
Reviewed-by: Zhang Lubo <lubo.zhang@intel.com>

MdeModulePkg/Universal/Network/UefiPxeBcDxe/PxeBcSupport.c
@@ -42,7 +42,7 @@ PxeBcCommonNotify (
   @param  SubnetMask Pointer to the subnetmask of the station ip address.
   @param  Gateway    Pointer to the gateway ip address.
   @param  SrcPort    Pointer to the srouce port of the station.
-  @param  TTL        The time to live field of the IP header. 
+  @param  Ttl        The time to live field of the IP header. 
   @param  ToS        The type of service field of the IP header.
 
   @retval EFI_SUCCESS           The configuration settings were set, changed, or reset successfully.
@@ -68,7 +68,7 @@ PxeBcConfigureUdpWriteInstance (
   IN EFI_IPv4_ADDRESS   *SubnetMask,
   IN EFI_IPv4_ADDRESS   *Gateway,
   IN OUT UINT16         *SrcPort,
-  IN     UINT8          TTL,
+  IN     UINT8          Ttl,
   IN     UINT8          ToS
   )
 {

MdeModulePkg/Universal/Network/UefiPxeBcDxe/PxeBcSupport.h
@@ -38,7 +38,7 @@ PxeBcCommonNotify (
   @param  SubnetMask Pointer to the subnetmask of the station ip address.
   @param  Gateway    Pointer to the gateway ip address.
   @param  SrcPort    Pointer to the srouce port of the station.
-  @param  TTL        The time to live field of the IP header. 
+  @param  Ttl        The time to live field of the IP header. 
   @param  ToS        The type of service field of the IP header.
 
   @retval EFI_SUCCESS           The configuration settings were set, changed, or reset successfully.
@@ -64,7 +64,7 @@ PxeBcConfigureUdpWriteInstance (
   IN EFI_IPv4_ADDRESS   *SubnetMask,
   IN EFI_IPv4_ADDRESS   *Gateway,
   IN OUT UINT16         *SrcPort,
-  IN     UINT8          TTL,
+  IN     UINT8          Ttl,
   IN     UINT8          ToS
   );
 /**

commit a0d35671652614968195314cdf6542c0a524b328

MdeModulePkg: Fix build error in the PXE driver.
Fix the build error introduced by e7fc78fdd61fe930c1ac775ba8c89e6c3ff871c5.

Contributed-under: TianoCore Contribution Agreement 1.0
Signed-off-by: Fu Siyuan <siyuan.fu@intel.com>

MdeModulePkg/Universal/Network/UefiPxeBcDxe/PxeBcSupport.c
@@ -79,7 +79,7 @@ PxeBcConfigureUdpWriteInstance (
 
   Udp4CfgData.ReceiveTimeout = PXEBC_DEFAULT_LIFETIME;
   Udp4CfgData.TypeOfService  = ToS;
-  Udp4CfgData.TimeToLive     = TTL;
+  Udp4CfgData.TimeToLive     = Ttl;
   Udp4CfgData.AllowDuplicatePort = TRUE;
 
   CopyMem (&Udp4CfgData.StationAddress, StationIp, sizeof (*StationIp));