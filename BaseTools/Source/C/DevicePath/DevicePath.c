Homerlv/edk2

commit 3ab4891b75b413319b6adef48f23064adc693d48

BaseTools: Add DevicePath support for PCD values
Use C code parse device path to output hex string, and Python
run command when PCD Value need device path parse.

https://bugzilla.tianocore.org/show_bug.cgi?id=541

Contributed-under: TianoCore Contribution Agreement 1.1
Signed-off-by: Yunhua Feng <yunhuax.feng@intel.com>
Signed-off-by: Yonghong Zhu <yonghong.zhu@intel.com>
Reviewed-by: Liming Gao <liming.gao@intel.com>

BaseTools/Source/C/DevicePath/DevicePath.c
@@ -1,0 +1,186 @@
+/** @file
+  Definition for Device Path Tool.
+
+Copyright (c) 2017, Intel Corporation. All rights reserved.<BR>
+This program and the accompanying materials
+are licensed and made available under the terms and conditions of the BSD License
+which accompanies this distribution.  The full text of the license may be found at
+http://opensource.org/licenses/bsd-license.php
+
+THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+
+**/
+
+#include "UefiDevicePathLib.h"
+
+//
+// Utility Name
+//
+#define UTILITY_NAME  "DevicePath"
+
+//
+// Utility version information
+//
+#define UTILITY_MAJOR_VERSION 0
+#define UTILITY_MINOR_VERSION 1
+
+EFI_GUID gEfiDebugPortDevicePathGuid = DEVICE_PATH_MESSAGING_DEBUGPORT;
+EFI_GUID gEfiPcAnsiGuid = EFI_PC_ANSI_GUID;
+EFI_GUID gEfiVT100Guid = EFI_VT_100_GUID;
+EFI_GUID gEfiVT100PlusGuid = EFI_VT_100_PLUS_GUID;
+EFI_GUID gEfiVTUTF8Guid = EFI_VT_UTF8_GUID;
+EFI_GUID gEfiUartDevicePathGuid = EFI_UART_DEVICE_PATH_GUID;
+EFI_GUID gEfiSasDevicePathGuid = EFI_SAS_DEVICE_PATH_GUID;
+EFI_GUID gEfiVirtualDiskGuid = EFI_VIRTUAL_DISK_GUID;
+EFI_GUID gEfiVirtualCdGuid = EFI_VIRTUAL_CD_GUID;
+EFI_GUID gEfiPersistentVirtualDiskGuid = EFI_PERSISTENT_VIRTUAL_DISK_GUID;
+EFI_GUID gEfiPersistentVirtualCdGuid = EFI_PERSISTENT_VIRTUAL_CD_GUID;
+
+STATIC
+VOID
+Version (
+  VOID
+)
+/*++
+
+Routine Description:
+
+  Displays the standard utility information to SDTOUT
+
+Arguments:
+
+  None
+
+Returns:
+
+  None
+
+--*/
+{
+  fprintf (stdout, "%s Version %d.%d %s \n", UTILITY_NAME, UTILITY_MAJOR_VERSION, UTILITY_MINOR_VERSION, __BUILD_VERSION);
+}
+
+STATIC
+VOID
+Usage (
+  VOID
+  )
+/*++
+
+Routine Description:
+
+  Displays the utility usage syntax to STDOUT
+
+Arguments:
+
+  None
+
+Returns:
+
+  None
+
+--*/
+{
+  //
+  // Summary usage
+  //
+  fprintf (stdout, "\nUsage: %s [options]\n\n", UTILITY_NAME);
+
+  //
+  // Copyright declaration
+  //
+  fprintf (stdout, "Copyright (c) 2017, Intel Corporation. All rights reserved.\n\n");
+  //
+  // Details Option
+  //
+  fprintf (stdout, "Options:\n");
+  fprintf (stdout, "  DevicePathString      Device Path string is specified, no space character.\n"
+                   "                        Example: \"PciRoot(0)/Pci(0,0)\"\n");
+
+  fprintf (stdout, "  --version             Show program's version number and exit.\n");
+  fprintf (stdout, "  -h, --help            Show this help message and exit.\n");
+}
+
+
+void print_mem(void const *vp, size_t n)
+{
+    unsigned char const *p = vp;
+    for (size_t i=0; i<n; i++) {
+        printf("0x%02x ", p[i]);
+  }
+}
+
+VOID
+Ascii2UnicodeString (
+  CHAR8    *String,
+  CHAR16   *UniString
+ )
+/*++
+
+Routine Description:
+
+  Write ascii string as unicode string format to FILE
+
+Arguments:
+
+  String      - Pointer to string that is written to FILE.
+  UniString   - Pointer to unicode string
+
+Returns:
+
+  NULL
+
+--*/
+{
+  while (*String != '\0') {
+    *(UniString++) = (CHAR16) *(String++);
+  }
+  //
+  // End the UniString with a NULL.
+  //
+  *UniString = '\0';
+}
+
+int main(int argc, CHAR8 *argv[])
+{
+  CHAR8 * Str;
+  CHAR16* Str16;
+  EFI_DEVICE_PATH_PROTOCOL *DevicePath;
+
+  if (argc == 1) {
+    Error (NULL, 0, 1001, "Missing options", "No input options specified.");
+    Usage ();
+    return STATUS_ERROR;
+  }
+  if ((stricmp (argv[1], "-h") == 0) || (stricmp (argv[1], "--help") == 0)) {
+    Version ();
+    Usage ();
+    return STATUS_SUCCESS;
+  }
+
+  if (stricmp (argv[1], "--version") == 0) {
+    Version ();
+    return STATUS_SUCCESS;
+  }
+  Str = argv[1];
+  if (Str == NULL) {
+    fprintf(stderr, "Invalid option value, Device Path can't be NULL");
+    return STATUS_ERROR;
+  }
+  Str16 = (CHAR16 *)malloc(1024);
+  if (Str16 == NULL) {
+    fprintf(stderr, "Resource, memory cannot be allcoated");
+    return STATUS_ERROR;
+  }
+  Ascii2UnicodeString(Str, Str16);
+  DevicePath = UefiDevicePathLibConvertTextToDevicePath(Str16);
+  while (!((DevicePath->Type == END_DEVICE_PATH_TYPE) && (DevicePath->SubType == END_ENTIRE_DEVICE_PATH_SUBTYPE)) )
+  {
+    print_mem(DevicePath, (DevicePath->Length[0] | DevicePath->Length[1] << 8));
+    DevicePath = (EFI_DEVICE_PATH_PROTOCOL *)((UINT8 *)DevicePath + (DevicePath->Length[0] | DevicePath->Length[1] << 8));
+  }
+  print_mem(DevicePath, (DevicePath->Length[0] | DevicePath->Length[1] << 8));
+  putchar('\n');
+  return STATUS_SUCCESS;
+}

BaseTools/BinWrappers/PosixLike/DevicePath
@@ -1,0 +1,29 @@
+#!/usr/bin/env bash
+
+full_cmd=${BASH_SOURCE:-$0} # see http://mywiki.wooledge.org/BashFAQ/028 for a discussion of why $0 is not a good choice here
+dir=$(dirname "$full_cmd")
+cmd=${full_cmd##*/}
+
+if [ -n "$WORKSPACE" ] && [ -e "$WORKSPACE/Conf/BaseToolsCBinaries" ]
+then
+  exec "$WORKSPACE/Conf/BaseToolsCBinaries/$cmd"
+elif [ -n "$WORKSPACE" ] && [ -e "$EDK_TOOLS_PATH/Source/C" ]
+then
+  if [ ! -e "$EDK_TOOLS_PATH/Source/C/bin/$cmd" ]
+  then
+    echo "BaseTools C Tool binary was not found ($cmd)"
+    echo "You may need to run:"
+    echo "  make -C $EDK_TOOLS_PATH/Source/C"
+  else
+    exec "$EDK_TOOLS_PATH/Source/C/bin/$cmd" "$@"
+  fi
+elif [ -e "$dir/../../Source/C/bin/$cmd" ]
+then
+  exec "$dir/../../Source/C/bin/$cmd" "$@"
+else
+  echo "Unable to find the real '$cmd' to run"
+  echo "This message was printed by"
+  echo "  $0"
+  exit 127
+fi
+

BaseTools/Source/C/Common/CommonLib.h
@@ -1,7 +1,7 @@
 /** @file
 Common library assistance routines.
 
-Copyright (c) 2004 - 2014, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2004 - 2017, Intel Corporation. All rights reserved.<BR>
 This program and the accompanying materials
 are licensed and made available under the terms and conditions of the BSD License
 which accompanies this distribution.  The full text of the license may be found at
@@ -17,9 +17,30 @@ WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
 
 #include <Common/UefiBaseTypes.h>
 #include <Common/BuildVersion.h>
+#include <assert.h>
 #define PRINTED_GUID_BUFFER_SIZE  37  // including null-termination
 
 #define MAX_LONG_FILE_PATH 500
+
+#define MAX_UINTN MAX_ADDRESS
+#define MAX_UINT64 ((UINT64)0xFFFFFFFFFFFFFFFFULL)
+#define MAX_UINT16  ((UINT16)0xFFFF)
+#define MAX_UINT8   ((UINT8)0xFF)
+#define ARRAY_SIZE(Array) (sizeof (Array) / sizeof ((Array)[0]))
+#define ASCII_RSIZE_MAX 1000000
+#ifndef RSIZE_MAX
+#define RSIZE_MAX 1000000
+#endif
+
+#define IS_COMMA(a)                ((a) == L',')
+#define IS_HYPHEN(a)               ((a) == L'-')
+#define IS_DOT(a)                  ((a) == L'.')
+#define IS_LEFT_PARENTH(a)         ((a) == L'(')
+#define IS_RIGHT_PARENTH(a)        ((a) == L')')
+#define IS_SLASH(a)                ((a) == L'/')
+#define IS_NULL(a)                 ((a) == L'\0')
+
+#define ASSERT(x) assert(x)
 
 #ifdef __cplusplus
 extern "C" {
@@ -149,6 +170,285 @@ CHAR8 *
 LongFilePath (
  IN CHAR8 *FileName
 );
+
+UINTN
+StrLen (
+  CONST CHAR16   *String
+  );
+
+VOID *
+AllocateCopyPool (
+  UINTN       AllocationSize,
+  CONST VOID  *Buffer
+  );
+
+INTN
+StrnCmp (
+  CONST CHAR16              *FirstString,
+  CONST CHAR16              *SecondString,
+  UINTN                     Length
+  );
+
+RETURN_STATUS
+StrToGuid (
+  CONST CHAR16       *String,
+  EFI_GUID               *Guid
+  );
+
+RETURN_STATUS
+StrHexToBytes (
+  CONST CHAR16       *String,
+  UINTN              Length,
+  UINT8              *Buffer,
+  UINTN              MaxBufferSize
+  );
+
+UINTN
+InternalHexCharToUintn (
+  CHAR16                    Char
+  );
+
+VOID *
+InternalAllocateCopyPool (
+   UINTN            AllocationSize,
+   CONST VOID       *Buffer
+  );
+
+BOOLEAN
+InternalIsDecimalDigitCharacter (
+        CHAR16                    Char
+  );
+
+UINT32
+SwapBytes32 (
+        UINT32                    Value
+  );
+
+UINT16
+SwapBytes16 (
+        UINT16                    Value
+  );
+
+EFI_GUID *
+CopyGuid (
+   EFI_GUID       *DestinationGuid,
+   CONST EFI_GUID  *SourceGuid
+  );
+
+UINT64
+WriteUnaligned64 (
+   UINT64                    *Buffer,
+   UINT64                    Value
+  );
+
+UINT64
+ReadUnaligned64 (
+   CONST UINT64              *Buffer
+  );
+
+UINTN
+StrSize (
+  CONST CHAR16              *String
+  );
+
+UINTN
+StrHexToUintn (
+  CONST CHAR16              *String
+  );
+
+UINTN
+StrDecimalToUintn (
+  CONST CHAR16              *String
+  );
+
+UINT64
+StrHexToUint64 (
+  CONST CHAR16             *String
+  );
+
+UINT64
+StrDecimalToUint64 (
+  CONST CHAR16              *String
+  );
+
+RETURN_STATUS
+StrHexToUint64S (
+    CONST CHAR16       *String,
+    CHAR16             **EndPointer,
+    UINT64             *Data
+  );
+
+RETURN_STATUS
+StrHexToUintnS (
+    CONST CHAR16             *String,
+         CHAR16             **EndPointer,  OPTIONAL
+         UINTN              *Data
+  );
+
+RETURN_STATUS
+StrDecimalToUint64S (
+    CONST CHAR16             *String,
+         CHAR16             **EndPointer,  OPTIONAL
+         UINT64             *Data
+  );
+
+RETURN_STATUS
+StrDecimalToUintnS (
+    CONST CHAR16             *String,
+         CHAR16             **EndPointer,  OPTIONAL
+         UINTN              *Data
+  );
+
+VOID *
+ReallocatePool (
+   UINTN  OldSize,
+   UINTN  NewSize,
+   VOID   *OldBuffer  OPTIONAL
+  );
+
+VOID *
+InternalReallocatePool (
+   UINTN            OldSize,
+   UINTN            NewSize,
+   VOID             *OldBuffer  OPTIONAL
+  );
+
+VOID *
+InternalAllocateZeroPool (
+   UINTN            AllocationSize
+  ) ;
+
+VOID *
+InternalAllocatePool (
+   UINTN            AllocationSize
+  );
+
+UINTN
+StrnLenS (
+   CONST CHAR16              *String,
+   UINTN                     MaxSize
+  );
+
+CHAR16
+InternalCharToUpper (
+        CHAR16                    Char
+  );
+
+INTN
+StrCmp (
+  CONST CHAR16              *FirstString,
+  CONST CHAR16              *SecondString
+  );
+
+UINT64
+SwapBytes64 (
+  UINT64                    Value
+  );
+
+UINT64
+InternalMathSwapBytes64 (
+  UINT64                    Operand
+  );
+
+RETURN_STATUS
+StrToIpv4Address (
+  CONST CHAR16       *String,
+  CHAR16             **EndPointer,
+  EFI_IPv4_ADDRESS       *Address,
+  UINT8              *PrefixLength
+  );
+
+RETURN_STATUS
+StrToIpv6Address (
+  CONST CHAR16       *String,
+  CHAR16             **EndPointer,
+  EFI_IPv6_ADDRESS       *Address,
+  UINT8              *PrefixLength
+  );
+
+RETURN_STATUS
+StrCpyS (
+  CHAR16       *Destination,
+  UINTN        DestMax,
+  CONST CHAR16 *Source
+  );
+
+RETURN_STATUS
+UnicodeStrToAsciiStrS (
+  CONST CHAR16              *Source,
+  CHAR8                     *Destination,
+  UINTN                     DestMax
+  );
+VOID *
+AllocatePool (
+  UINTN  AllocationSize
+  );
+
+UINT16
+WriteUnaligned16 (
+  UINT16                    *Buffer,
+  UINT16                    Value
+  );
+
+UINT16
+ReadUnaligned16 (
+  CONST UINT16              *Buffer
+  );
+
+VOID *
+AllocateZeroPool (
+  UINTN  AllocationSize
+  );
+
+BOOLEAN
+InternalIsHexaDecimalDigitCharacter (
+  CHAR16                    Char
+  );
+
+BOOLEAN
+InternalSafeStringIsOverlap (
+  IN VOID    *Base1,
+  IN UINTN   Size1,
+  IN VOID    *Base2,
+  IN UINTN   Size2
+  );
+
+BOOLEAN
+InternalSafeStringNoStrOverlap (
+  IN CHAR16  *Str1,
+  IN UINTN   Size1,
+  IN CHAR16  *Str2,
+  IN UINTN   Size2
+  );
+
+BOOLEAN
+IsHexStr (
+   CHAR16   *Str
+  );
+
+UINTN
+Strtoi (
+   CHAR16  *Str
+  );
+
+VOID
+Strtoi64 (
+    CHAR16  *Str,
+   UINT64  *Data
+  );
+
+VOID
+StrToAscii (
+       CHAR16 *Str,
+    CHAR8  **AsciiStr
+  );
+
+CHAR16 *
+SplitStr (
+    CHAR16 **List,
+       CHAR16 Separator
+  );
+
 /*++
 
 Routine Description:
@@ -165,8 +465,6 @@ Returns:
 #ifdef __cplusplus
 }
 #endif
-
-#define ASSERT(x) assert(x)
 
 #ifdef __GNUC__
 #include <stdio.h>

BaseTools/Source/C/DevicePath/GNUmakefile
@@ -1,0 +1,30 @@
+## @file
+# GNU/Linux makefile for 'DevicePath' module build.
+#
+# Copyright (c) 2017, Intel Corporation. All rights reserved.<BR>
+# This program and the accompanying materials
+# are licensed and made available under the terms and conditions of the BSD License
+# which accompanies this distribution.  The full text of the license may be found at
+# http://opensource.org/licenses/bsd-license.php
+#
+# THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+# WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+#
+ARCH ?= IA32
+MAKEROOT ?= ..
+
+APPNAME = DevicePath
+
+OBJECTS = DevicePath.o UefiDevicePathLib.o DevicePathFromText.o  DevicePathUtilities.o
+
+include $(MAKEROOT)/Makefiles/app.makefile
+
+LIBS = -lCommon
+ifeq ($(CYGWIN), CYGWIN)
+  LIBS += -L/lib/e2fsprogs -luuid
+endif
+
+ifeq ($(LINUX), Linux)
+  LIBS += -luuid
+endif
+

BaseTools/Source/C/DevicePath/Makefile
@@ -1,0 +1,24 @@
+## @file
+# Windows makefile for 'DevicePath' module build.
+#
+# Copyright (c) 2017, Intel Corporation. All rights reserved.<BR>
+# This program and the accompanying materials
+# are licensed and made available under the terms and conditions of the BSD License
+# which accompanies this distribution.  The full text of the license may be found at
+# http://opensource.org/licenses/bsd-license.php
+#
+# THE PROGRAM IS DISTRIBUTED UNDER THE BSD LICENSE ON AN "AS IS" BASIS,
+# WITHOUT WARRANTIES OR REPRESENTATIONS OF ANY KIND, EITHER EXPRESS OR IMPLIED.
+#
+!INCLUDE ..\Makefiles\ms.common
+
+APPNAME = DevicePath
+
+LIBS = $(LIB_PATH)\Common.lib
+
+OBJECTS = DevicePath.obj UefiDevicePathLib.obj DevicePathFromText.obj  DevicePathUtilities.obj
+
+#CFLAGS = $(CFLAGS) /nodefaultlib:libc.lib
+
+!INCLUDE ..\Makefiles\ms.app
+

commit af999c3ed2111a17f2493cac798ebb2caa1eac5d

BaseTools/DevicePath: fix GCC build error in print_mem(), and clean it up
Currently "BaseTools/Source/C/DevicePath/DevicePath.c" fails to build with
GCC48:

> DevicePath.c: In function 'print_mem':
> DevicePath.c:109:5: error: 'for' loop initial declarations are only
> allowed in C99 mode
>      for (size_t i=0; i<n; i++) {
>      ^
> DevicePath.c:109:5: note: use option -std=c99 or -std=gnu99 to compile
> your code

In addition, the print_mem() function does not conform to the edk2 coding
style:

- we use CamelCase and no underscores in identifiers,
- the types and type qualifiers should follow the edk2 style,
- initialization as part of definition is forbidden for local variables.

Clean these up.

While updating the print_mem()/PrintMem() call sites, also remove the
superfluous parentheses around the second argument.

Cc: Liming Gao <liming.gao@intel.com>
Cc: Yonghong Zhu <yonghong.zhu@intel.com>
Fixes: 3ab4891b75b413319b6adef48f23064adc693d48
Contributed-under: TianoCore Contribution Agreement 1.1
Signed-off-by: Laszlo Ersek <lersek@redhat.com>
Reviewed-by: Yonghong Zhu <yonghong.zhu@intel.com>

BaseTools/Source/C/DevicePath/DevicePath.c
@@ -103,11 +103,19 @@ Returns:
 }
 
 
-void print_mem(void const *vp, size_t n)
+STATIC
+VOID
+PrintMem (
+  CONST VOID *Buffer,
+  UINTN      Count
+  )
 {
-    unsigned char const *p = vp;
-    for (size_t i=0; i<n; i++) {
-        printf("0x%02x ", p[i]);
+  CONST UINT8 *Bytes;
+  UINTN       Idx;
+
+  Bytes = Buffer;
+  for (Idx = 0; Idx < Count; Idx++) {
+    printf("0x%02x ", Bytes[Idx]);
   }
 }
 
@@ -177,10 +185,10 @@ int main(int argc, CHAR8 *argv[])
   DevicePath = UefiDevicePathLibConvertTextToDevicePath(Str16);
   while (!((DevicePath->Type == END_DEVICE_PATH_TYPE) && (DevicePath->SubType == END_ENTIRE_DEVICE_PATH_SUBTYPE)) )
   {
-    print_mem(DevicePath, (DevicePath->Length[0] | DevicePath->Length[1] << 8));
+    PrintMem (DevicePath, DevicePath->Length[0] | DevicePath->Length[1] << 8);
     DevicePath = (EFI_DEVICE_PATH_PROTOCOL *)((UINT8 *)DevicePath + (DevicePath->Length[0] | DevicePath->Length[1] << 8));
   }
-  print_mem(DevicePath, (DevicePath->Length[0] | DevicePath->Length[1] << 8));
+  PrintMem (DevicePath, DevicePath->Length[0] | DevicePath->Length[1] << 8);
   putchar('\n');
   return STATUS_SUCCESS;
 }