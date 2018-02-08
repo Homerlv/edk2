Homerlv/edk2

commit de503d71a023e58a0fe309e13b8e35883fe71d02

ShellPkg/UefiShellDebug1CommandsLib: Remove unnecessary EFIAPI
Contributed-under: TianoCore Contribution Agreement 1.0
Signed-off-by: Chen A Chen <chen.a.chen@intel.com>
Reviewed-by: Jaben Carsey <jaben.carsey@intel.com>
Reviewed-by: Ruiyu Ni <Ruiyu.ni@intel.com>

ShellPkg/Library/UefiShellDebug1CommandsLib/Compress.c
@@ -68,7 +68,6 @@ typedef INT16             NODE;
   @param[in] Data    The dword to put.
 **/
 VOID
-EFIAPI
 PutDword(
   IN UINT32 Data
   );
@@ -128,7 +127,6 @@ INT32         mHuffmanDepth = 0;
 
 **/
 VOID
-EFIAPI
 MakeCrcTable (
   VOID
   )
@@ -159,7 +157,6 @@ MakeCrcTable (
   @param[in] Data    The dword to put.
 **/
 VOID
-EFIAPI
 PutDword (
   IN UINT32 Data
   )
@@ -188,7 +185,6 @@ PutDword (
   @retval EFI_OUT_OF_RESOURCES  A memory allocation failed.
 **/
 EFI_STATUS
-EFIAPI
 AllocateMemory (
   VOID
   )
@@ -222,7 +218,6 @@ AllocateMemory (
 
 **/
 VOID
-EFIAPI
 FreeMemory (
   VOID
   )
@@ -241,7 +236,6 @@ FreeMemory (
   Initialize String Info Log data structures.
 **/
 VOID
-EFIAPI
 InitSlide (
   VOID
   )
@@ -273,7 +267,6 @@ InitSlide (
 
 **/
 NODE
-EFIAPI
 Child (
   IN NODE   LoopVar6,
   IN UINT8  LoopVar5
@@ -298,7 +291,6 @@ Child (
   @param[in] LoopVar4       The child node.
 **/
 VOID
-EFIAPI
 MakeChild (
   IN NODE   LoopVar6,
   IN UINT8  LoopVar5,
@@ -325,7 +317,6 @@ MakeChild (
   @param[in] Old     The node to split.
 **/
 VOID
-EFIAPI
 Split (
   IN NODE Old
   )
@@ -355,7 +346,6 @@ Split (
 
 **/
 VOID
-EFIAPI
 InsertNode (
   VOID
   )
@@ -484,7 +474,6 @@ InsertNode (
 
 **/
 VOID
-EFIAPI
 DeleteNode (
   VOID
   )
@@ -579,7 +568,6 @@ DeleteNode (
   @return The number of bytes actually read.
 **/
 INT32
-EFIAPI
 FreadCrc (
   OUT UINT8 *LoopVar7,
   IN  INT32 LoopVar8
@@ -612,7 +600,6 @@ FreadCrc (
   @retval FALSE     The operation failed due to insufficient memory.
 **/
 BOOLEAN
-EFIAPI
 GetNextMatch (
   VOID
   )
@@ -647,7 +634,6 @@ GetNextMatch (
   @param[in] LoopVar1    The index of the item to move.
 **/
 VOID
-EFIAPI
 DownHeap (
   IN INT32 i
   )
@@ -684,7 +670,6 @@ DownHeap (
   @param[in] LoopVar1      The top node.
 **/
 VOID
-EFIAPI
 CountLen (
   IN INT32 LoopVar1
   )
@@ -705,7 +690,6 @@ CountLen (
   @param[in] Root   The root of the tree.
 **/
 VOID
-EFIAPI
 MakeLen (
   IN INT32 Root
   )
@@ -761,7 +745,6 @@ MakeLen (
   @param[out] Code  The stores codes for each symbol.
 **/
 VOID
-EFIAPI
 MakeCode (
   IN  INT32         LoopVar8,
   IN  UINT8 Len[    ],
@@ -792,7 +775,6 @@ MakeCode (
   @return The root of the Huffman tree.
 **/
 INT32
-EFIAPI
 MakeTree (
   IN  INT32             NParm,
   IN  UINT16  FreqParm[ ],
@@ -876,7 +858,6 @@ MakeTree (
   @param[in] x   The data.
 **/
 VOID
-EFIAPI
 PutBits (
   IN INT32    LoopVar8,
   IN UINT32   x
@@ -915,7 +896,6 @@ PutBits (
   @param[in] LoopVar5     The number to encode.
 **/
 VOID
-EFIAPI
 EncodeC (
   IN INT32 LoopVar5
   )
@@ -929,7 +909,6 @@ EncodeC (
   @param[in] LoopVar7     The number to encode.
 **/
 VOID
-EFIAPI
 EncodeP (
   IN UINT32 LoopVar7
   )
@@ -956,7 +935,6 @@ EncodeP (
 
 **/
 VOID
-EFIAPI
 CountTFreq (
   VOID
   )
@@ -1014,7 +992,6 @@ CountTFreq (
 
 **/
 VOID
-EFIAPI
 WritePTLen (
   IN INT32 LoopVar8,
   IN INT32 nbit,
@@ -1053,7 +1030,6 @@ WritePTLen (
   Outputs the code length array for Char&Length Set.
 **/
 VOID
-EFIAPI
 WriteCLen (
   VOID
   )
@@ -1109,7 +1085,6 @@ WriteCLen (
 
 **/
 VOID
-EFIAPI
 SendBlock (
   VOID
   )
@@ -1183,7 +1158,6 @@ SendBlock (
 
 **/
 VOID
-EFIAPI
 HufEncodeStart (
   VOID
   )
@@ -1205,7 +1179,6 @@ HufEncodeStart (
   @param[in] LoopVar7     The 'Position' field of a Pointer.
 **/
 VOID
-EFIAPI
 CompressOutput (
   IN UINT32 LoopVar5,
   IN UINT32 LoopVar7
@@ -1243,7 +1216,6 @@ CompressOutput (
 
 **/
 VOID
-EFIAPI
 HufEncodeEnd (
   VOID
   )
@@ -1263,7 +1235,6 @@ HufEncodeEnd (
   @retval EFI_OUT_0F_RESOURCES  Not enough memory for compression process.
 **/
 EFI_STATUS
-EFIAPI
 Encode (
   VOID
   )
@@ -1346,7 +1317,6 @@ Encode (
   @retval EFI_BUFFER_TOO_SMALL  The buffer was too small.  DstSize is required.
 **/
 EFI_STATUS
-EFIAPI
 Compress (
   IN       VOID   *SrcBuffer,
   IN       UINT64 SrcSize,

ShellPkg/Library/UefiShellDebug1CommandsLib/Dblk.c
@@ -26,7 +26,6 @@
   @retval SHELL_SUCCESS   The display was successful.
 **/
 SHELL_STATUS
-EFIAPI
 DisplayTheBlocks(
   IN CONST EFI_DEVICE_PATH_PROTOCOL *DevPath,
   IN CONST UINT64                   Lba,

ShellPkg/Library/UefiShellDebug1CommandsLib/Dmem.c
@@ -31,7 +31,6 @@
   @return A printable character representing Char.
 **/
 CHAR16
-EFIAPI
 MakePrintable(
   IN CONST CHAR16 Char
   )
@@ -49,7 +48,6 @@ MakePrintable(
   @param[in] Size       The length of memory to display.
 **/
 SHELL_STATUS
-EFIAPI
 DisplayMmioMemory(
   IN CONST VOID   *Address,
   IN CONST UINTN  Size

ShellPkg/Library/UefiShellDebug1CommandsLib/DmpStore.c
@@ -42,7 +42,6 @@ typedef struct {
   @retval The attribute string info.
 **/
 CHAR16 *
-EFIAPI
 GetAttrType (
   IN CONST UINT32 Atts
   )
@@ -367,7 +366,6 @@ AppendSingleVariableToFile (
   @retval SHELL_NOT_FOUND         the Name/Guid pair could not be found.
 **/
 SHELL_STATUS
-EFIAPI
 CascadeProcessVariables (
   IN CONST CHAR16      *Name        OPTIONAL,
   IN CONST EFI_GUID    *Guid        OPTIONAL,
@@ -537,7 +535,6 @@ CascadeProcessVariables (
   @retval SHELL_NOT_FOUND         the Name/Guid pair could not be found.
 **/
 SHELL_STATUS
-EFIAPI
 ProcessVariables (
   IN CONST CHAR16      *Name      OPTIONAL,
   IN CONST EFI_GUID    *Guid      OPTIONAL,

ShellPkg/Library/UefiShellDebug1CommandsLib/Edit/FileBuffer.c
@@ -72,7 +72,6 @@ extern BOOLEAN          EditorMouseAction;
   @param EFI_OUT_OF_RESOURCES   A memory allocation failed.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferInit (
   VOID
   )
@@ -121,7 +120,6 @@ FileBufferInit (
   @retval EFI_SUCCESS           The backup operation was successful.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferBackup (
   VOID
   )
@@ -155,7 +153,6 @@ FileBufferBackup (
   @return  The line structure after the advance.
 **/
 EFI_EDITOR_LINE *
-EFIAPI
 InternalEditorMiscLineAdvance (
   IN CONST UINTN            Count,
   IN CONST EFI_EDITOR_LINE  *CurrentLine,
@@ -195,7 +192,6 @@ InternalEditorMiscLineAdvance (
   @return  The line structure after the retreat.
 **/
 EFI_EDITOR_LINE *
-EFIAPI
 InternalEditorMiscLineRetreat (
   IN CONST UINTN            Count,
   IN CONST EFI_EDITOR_LINE  *CurrentLine,
@@ -262,7 +258,6 @@ MoveLine (
   @retval EFI_SUCCESS           The backup operation was successful.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferRestoreMousePosition (
   VOID
   )
@@ -406,7 +401,6 @@ FileBufferRestoreMousePosition (
   @retval EFI_SUCCESS     The operation was successful.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferFreeLines (
   VOID
   )
@@ -450,7 +444,6 @@ FileBufferFreeLines (
   @retval EFI_SUCCESS   The cleanup was successful.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferCleanup (
   VOID
   )
@@ -537,7 +530,6 @@ FileBufferPrintLine (
   @retval EFI_SUCCESS           The operation was successful.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferRestorePosition (
   VOID
   )
@@ -559,7 +551,6 @@ FileBufferRestorePosition (
   @retval EFI_LOAD_ERROR  There was an error finding what to write.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferRefresh (
   VOID
   )
@@ -671,7 +662,6 @@ FileBufferRefresh (
   @return         The line created.
 **/
 EFI_EDITOR_LINE *
-EFIAPI
 FileBufferCreateLine (
   VOID
   )
@@ -726,7 +716,6 @@ FileBufferCreateLine (
   @retval EFI_INVALID_PARAMETER Str is not a valid filename.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferSetFileName (
   IN CONST CHAR16 *Str
   )
@@ -758,7 +747,6 @@ FileBufferSetFileName (
   @retval EFI_SUCCESS           The operation was successful.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferFree (
   VOID
   )
@@ -785,7 +773,6 @@ FileBufferFree (
   @retval EFI_INVALID_PARAMETER  FileName is a directory.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferRead (
   IN CONST CHAR16  *FileName,
   IN CONST BOOLEAN Recover
@@ -1280,7 +1267,6 @@ Done:
   @param[out] Size              The amount of the buffer used on return.
 **/
 VOID
-EFIAPI
 GetNewLine (
   IN CONST EE_NEWLINE_TYPE Type,
   OUT CHAR8           *Buffer,
@@ -1387,7 +1373,6 @@ GetNewLine (
   @return The actuall length.
 **/
 UINTN
-EFIAPI
 UnicodeToAscii (
   IN CONST CHAR16   *UStr,
   IN CONST UINTN    Length,
@@ -1416,7 +1401,6 @@ UnicodeToAscii (
   @retval EFI_OUT_OF_RESOURCES  There were not enough resources to write the file.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferSave (
   IN CONST CHAR16 *FileName
   )
@@ -1656,7 +1640,6 @@ FileBufferSave (
   @retval EFI_SUCCESS     The operation was successful.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferScrollLeft (
   VOID
   )
@@ -1703,7 +1686,6 @@ FileBufferScrollLeft (
   @param[in] Pos         Position to delete the char at ( start from 0 ).
 **/
 VOID
-EFIAPI
 LineDeleteAt (
   IN  OUT EFI_EDITOR_LINE       *Line,
   IN      UINTN                 Pos
@@ -1728,7 +1710,6 @@ LineDeleteAt (
   @param[in] Src         Src String.
 **/
 VOID
-EFIAPI
 LineCat (
   IN  OUT EFI_EDITOR_LINE *Dest,
   IN      EFI_EDITOR_LINE *Src
@@ -1769,7 +1750,6 @@ LineCat (
   @retval EFI_OUT_OF_RESOURCES  A memory allocation failed.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferDoBackspace (
   VOID
   )
@@ -1841,7 +1821,6 @@ FileBufferDoBackspace (
   @retval EFI_OUT_OF_RESOURCES  A memory allocation failed.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferDoReturn (
   VOID
   )
@@ -1938,7 +1917,6 @@ FileBufferDoReturn (
   @retval EFI_SUCCESS
 **/
 EFI_STATUS
-EFIAPI
 FileBufferDoDelete (
   VOID
   )
@@ -2001,7 +1979,6 @@ FileBufferDoDelete (
   @retval EFI_SUCCESS     The operation was successful.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferScrollRight (
   VOID
   )
@@ -2055,7 +2032,6 @@ FileBufferScrollRight (
   @return The new string size ( include CHAR_NULL ) ( unit is Unicode character ).
 **/
 UINTN
-EFIAPI
 LineStrInsert (
   IN      EFI_EDITOR_LINE  *Line,
   IN      CHAR16           Char,
@@ -2108,7 +2084,6 @@ LineStrInsert (
   @retval EFI_SUCCESS           The input was succesful.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferAddChar (
   IN  CHAR16  Char
   )
@@ -2156,7 +2131,6 @@ FileBufferAddChar (
   @retval EFI_OUT_OF_RESOURCES  A memory allocation failed.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferDoCharInput (
   IN CONST CHAR16 Char
   )
@@ -2207,7 +2181,6 @@ FileBufferDoCharInput (
   @retval EFI_SUCCESS     The operation was successful.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferScrollDown (
   VOID
   )
@@ -2253,7 +2226,6 @@ FileBufferScrollDown (
   @retval EFI_SUCCESS     The operation was successful.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferScrollUp (
   VOID
   )
@@ -2296,7 +2268,6 @@ FileBufferScrollUp (
   @retval EFI_SUCCESS     The operation wa successful.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferPageDown (
   VOID
   )
@@ -2347,7 +2318,6 @@ FileBufferPageDown (
   @retval EFI_SUCCESS     The operation was successful.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferPageUp (
   VOID
   )
@@ -2403,7 +2373,6 @@ FileBufferPageUp (
   @retval EFI_SUCCESS       The operation was successful.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferEnd (
   VOID
   )
@@ -2441,7 +2410,6 @@ FileBufferEnd (
   @retval EFI_OUT_OF_RESOURCES  A memory allocation failed.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferHandleInput (
   IN CONST EFI_INPUT_KEY *Key
   )
@@ -2557,7 +2525,6 @@ FileBufferHandleInput (
   @retval FALSE   It is not above the current screen.
 **/
 BOOLEAN
-EFIAPI
 AboveCurrentScreen (
   IN UINTN FileRow
   )
@@ -2581,7 +2548,6 @@ AboveCurrentScreen (
   @retval FALSE     It is not under the current screen.
 **/
 BOOLEAN
-EFIAPI
 UnderCurrentScreen (
   IN UINTN FileRow
   )
@@ -2605,7 +2571,6 @@ UnderCurrentScreen (
   @retval FALSE   It is not to the left.
 **/
 BOOLEAN
-EFIAPI
 LeftCurrentScreen (
   IN UINTN FileCol
   )
@@ -2629,7 +2594,6 @@ LeftCurrentScreen (
   @retval FALSE   It is not to the right.
 **/
 BOOLEAN
-EFIAPI
 RightCurrentScreen (
   IN UINTN FileCol
   )
@@ -2655,7 +2619,6 @@ RightCurrentScreen (
   @return The line after advance/retreat.
 **/
 EFI_EDITOR_LINE *
-EFIAPI
 MoveCurrentLine (
   IN  INTN Count
   )
@@ -2686,7 +2649,6 @@ MoveCurrentLine (
   @param[in] NewFilePosCol    The column of file position ( start from 1 ).
 **/
 VOID
-EFIAPI
 FileBufferMovePosition (
   IN CONST UINTN NewFilePosRow,
   IN CONST UINTN NewFilePosCol
@@ -2802,7 +2764,6 @@ FileBufferMovePosition (
   @retval EFI_OUT_OF_RESOURCES    A memory allocation failed.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferCutLine (
   OUT EFI_EDITOR_LINE **CutLine
   )
@@ -2882,7 +2843,6 @@ FileBufferCutLine (
   @retval EFI_OUT_OF_RESOURCES    A memory allocation failed.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferPasteLine (
   VOID
   )
@@ -2954,7 +2914,6 @@ FileBufferPasteLine (
   @retval EFI_NOT_FOUND     The string Str was not found.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferSearch (
   IN CONST CHAR16  *Str,
   IN CONST UINTN Offset
@@ -3060,7 +3019,6 @@ FileBufferSearch (
   @retval EFI_OUT_OF_RESOURCES    A memory allocation failed.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferReplace (
   IN CONST CHAR16   *Replace,
   IN CONST UINTN    SearchLen
@@ -3165,7 +3123,6 @@ FileBufferReplace (
   @param[in] TextY      The new y-coordinate.
 **/
 VOID
-EFIAPI
 FileBufferAdjustMousePosition (
   IN CONST INT32 TextX,
   IN CONST INT32 TextY
@@ -3242,7 +3199,6 @@ FileBufferAdjustMousePosition (
   @param[in] Offset       The column to start at.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferReplaceAll (
   IN CHAR16 *SearchStr,
   IN CHAR16 *ReplaceStr,
@@ -3360,7 +3316,6 @@ FileBufferReplaceAll (
   Set the modified state to TRUE.
 **/
 VOID
-EFIAPI
 FileBufferSetModified (
   VOID
   )

ShellPkg/Library/UefiShellDebug1CommandsLib/Edit/FileBuffer.h
@@ -25,7 +25,6 @@
   @param EFI_OUT_OF_RESOURCES   A memory allocation failed.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferInit (
   VOID
   );
@@ -36,7 +35,6 @@ FileBufferInit (
   @retval EFI_SUCCESS   The cleanup was successful.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferCleanup (
   VOID
   );
@@ -48,7 +46,6 @@ FileBufferCleanup (
   @retval EFI_LOAD_ERROR  There was an error finding what to write.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferRefresh (
   VOID
   );
@@ -68,7 +65,6 @@ FileBufferRefresh (
   @retval EFI_OUT_OF_RESOURCES  A memory allocation failed.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferHandleInput (
   IN CONST EFI_INPUT_KEY * Key
   );
@@ -84,7 +80,6 @@ FileBufferHandleInput (
   @retval EFI_SUCCESS           The backup operation was successful.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferBackup (
   VOID
   );
@@ -95,7 +90,6 @@ FileBufferBackup (
   @retval EFI_SUCCESS           The operation was successful.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferRestorePosition (
   VOID
   );
@@ -110,7 +104,6 @@ FileBufferRestorePosition (
   @retval EFI_INVALID_PARAMETER Str is not a valid filename.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferSetFileName (
   IN CONST CHAR16 *Str
   );
@@ -127,7 +120,6 @@ FileBufferSetFileName (
   @retval EFI_INVALID_PARAMETER  FileName is a directory.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferRead (
   IN CONST CHAR16  *FileName,
   IN CONST BOOLEAN Recover
@@ -143,7 +135,6 @@ FileBufferRead (
   @retval EFI_OUT_OF_RESOURCES  There were not enough resources to write the file.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferSave (
   CONST CHAR16 *FileName
   );
@@ -155,7 +146,6 @@ FileBufferSave (
   @param[in] NewFilePosCol    The column of file position ( start from 1 ).
 **/
 VOID
-EFIAPI
 FileBufferMovePosition (
   IN CONST UINTN NewFilePosRow,
   IN CONST UINTN NewFilePosCol
@@ -172,7 +162,6 @@ FileBufferMovePosition (
   @retval EFI_OUT_OF_RESOURCES    A memory allocation failed.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferCutLine (
   OUT EFI_EDITOR_LINE **CutLine
   );
@@ -184,7 +173,6 @@ FileBufferCutLine (
   @retval EFI_OUT_OF_RESOURCES    A memory allocation failed.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferPasteLine (
   VOID
   );
@@ -199,7 +187,6 @@ FileBufferPasteLine (
   @retval EFI_NOT_FOUND     The string Str was not found.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferSearch (
   IN CONST CHAR16  *Str,
   IN CONST UINTN Offset
@@ -217,7 +204,6 @@ FileBufferSearch (
   @retval EFI_OUT_OF_RESOURCES    A memory allocation failed.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferReplace (
   IN CONST CHAR16   *Replace,
   IN CONST UINTN    SearchLen
@@ -231,7 +217,6 @@ FileBufferReplace (
   @param[in] Offset       The column to start at.
 **/
 EFI_STATUS
-EFIAPI
 FileBufferReplaceAll (
   IN CHAR16 *SearchStr,
   IN CHAR16 *ReplaceStr,
@@ -245,7 +230,6 @@ FileBufferReplaceAll (
   @param[in] TextY      The new y-coordinate.
 **/
 VOID
-EFIAPI
 FileBufferAdjustMousePosition (
   IN CONST INT32 TextX,
   IN CONST INT32 TextY
@@ -255,7 +239,6 @@ FileBufferAdjustMousePosition (
   Set the modified state to TRUE.
 **/
 VOID
-EFIAPI
 FileBufferSetModified (
   VOID
   );

ShellPkg/Library/UefiShellDebug1CommandsLib/Edit/MainTextEditor.c
@@ -1418,7 +1418,6 @@ EFI_EDITOR_GLOBAL_EDITOR      MainEditorConst = {
   @retval EFI_LOAD_ERROR          A load error occured.
 **/
 EFI_STATUS
-EFIAPI
 MainEditorInit (
   VOID
   )
@@ -1552,7 +1551,6 @@ MainEditorInit (
   @retval EFI_LOAD_ERROR          A load error occured.
 **/
 EFI_STATUS
-EFIAPI
 MainEditorCleanup (
   VOID
   )
@@ -1596,7 +1594,6 @@ MainEditorCleanup (
   Refresh the main editor component.
 **/
 VOID
-EFIAPI
 MainEditorRefresh (
   VOID
   )
@@ -1649,7 +1646,6 @@ MainEditorRefresh (
   @return The X location of the mouse.
 **/
 INT32
-EFIAPI
 GetTextX (
   IN INT32 GuidX
   )
@@ -1671,7 +1667,6 @@ GetTextX (
   @return The Y location of the mouse.
 **/
 INT32
-EFIAPI
 GetTextY (
   IN INT32 GuidY
   )
@@ -1695,7 +1690,6 @@ GetTextY (
   @retval EFI_NOT_FOUND     There was no mouse support found.
 **/
 EFI_STATUS
-EFIAPI
 MainEditorHandleMouseInput (
   IN EFI_SIMPLE_POINTER_STATE       MouseState
   )
@@ -1796,7 +1790,6 @@ MainEditorHandleMouseInput (
   @retval EFI_OUT_OF_RESOURCES    A memory allocation failed.
 **/
 EFI_STATUS
-EFIAPI
 MainEditorKeyInput (
   VOID
   )
@@ -1893,7 +1886,6 @@ MainEditorKeyInput (
   @retval EFI_OUT_OF_RESOURCES A memory allocation failed.
 **/
 EFI_STATUS
-EFIAPI
 MainEditorSetCutLine (
   EFI_EDITOR_LINE *Line
   )
@@ -1925,7 +1917,6 @@ MainEditorSetCutLine (
   @retval EFI_SUCCESS The operation was successful.
 **/
 EFI_STATUS
-EFIAPI
 MainEditorBackup (
   VOID
   )

ShellPkg/Library/UefiShellDebug1CommandsLib/Edit/MainTextEditor.h
@@ -24,7 +24,6 @@
   @retval EFI_LOAD_ERROR          A load error occured.
 **/
 EFI_STATUS
-EFIAPI
 MainEditorInit (
   VOID
   );
@@ -36,7 +35,6 @@ MainEditorInit (
   @retval EFI_LOAD_ERROR          A load error occured.
 **/
 EFI_STATUS
-EFIAPI
 MainEditorCleanup (
   VOID
   );
@@ -45,7 +43,6 @@ MainEditorCleanup (
   Refresh the main editor component.
 **/
 VOID
-EFIAPI
 MainEditorRefresh (
   VOID
   );
@@ -58,7 +55,6 @@ MainEditorRefresh (
   @retval EFI_OUT_OF_RESOURCES    A memory allocation failed.
 **/
 EFI_STATUS
-EFIAPI
 MainEditorKeyInput (
   VOID
   );
@@ -69,7 +65,6 @@ MainEditorKeyInput (
   @retval EFI_SUCCESS The operation was successful.
 **/
 EFI_STATUS
-EFIAPI
 MainEditorBackup (
   VOID
   );

commit 468c7024ca9524adb169360d1a7551ebc1c5e934

ShellPkg/UefiShellDebug1CommandsLib: sync Compress() definition with decl.
"Compress.h" declares the Compress() function as EFIAPI, but the
definition in "Compress.c" lacks EFIAPI.

GCC toolchains without LTO do not catch this error because "Compress.c"
does not include "Compress.h"; i.e. the declaration used by callers such
as "EfiCompress.c" is not actually matched against the function definition
at build time.

With LTO enabled, the mismatch is found -- however, as a warning only, due
to commit 2da9b6f18dc4 ("BaseTools GCC5: disable warnings-as-errors for
now", 2016-08-03).

Include the header in the C file (which turns the issue into a hard build
error on all GCC toolchains), plus sync the declaration from the header
file to the C file. Finally, remove EFIAPI from both declaration and
definition -- this was the original intent of commit de503d71a023
("ShellPkg/UefiShellDebug1CommandsLib: Remove unnecessary EFIAPI",
2016-10-09), but it missed the header file.

(Gary meant to address that omission in Oct 2017:

  [edk2] [PATCH] ShellPkg/UefiShellDebug1CommandsLib: Remove EFIAPI from
                 Compress()

  http://mid.mail-archive.com/20171026065329.32311-1-glin@suse.com

and Ray reviewed the patch, but then the patch was never committed.)

So do the sync and drop EFIAPI now.

This happens to fix the EFICOMPRESS shell command, when built with GCC for
X64.

Cc: Gary Lin <glin@suse.com>
Cc: Jaben Carsey <jaben.carsey@intel.com>
Cc: Ruiyu Ni <ruiyu.ni@intel.com>
Fixes: de503d71a023e58a0fe309e13b8e35883fe71d02
Contributed-under: TianoCore Contribution Agreement 1.1
Signed-off-by: Laszlo Ersek <lersek@redhat.com>
Reviewed-by: Gary Lin <glin@suse.com>
Reviewed-by: Jaben Carsey <jaben.carsey@intel.com>

ShellPkg/Library/UefiShellDebug1CommandsLib/Compress.c
@@ -23,6 +23,8 @@
 #include <Library/DebugLib.h>
 #include <Library/ShellLib.h>
 
+#include "Compress.h"
+
 //
 // Macro Definitions
 //
@@ -1307,20 +1309,20 @@ Encode (
   The compression routine.
 
   @param[in]       SrcBuffer     The buffer containing the source data.
-  @param[in]       SrcSize       The number of bytes in SrcBuffer.
+  @param[in]       SrcSize       Number of bytes in SrcBuffer.
   @param[in]       DstBuffer     The buffer to put the compressed image in.
   @param[in, out]  DstSize       On input the size (in bytes) of DstBuffer, on
-                                return the number of bytes placed in DstBuffer.
+                                 return the number of bytes placed in DstBuffer.
 
   @retval EFI_SUCCESS           The compression was sucessful.
   @retval EFI_BUFFER_TOO_SMALL  The buffer was too small.  DstSize is required.
 **/
 EFI_STATUS
 Compress (
-  IN       VOID   *SrcBuffer,
-  IN       UINT64 SrcSize,
-  IN       VOID   *DstBuffer,
-  IN OUT   UINT64 *DstSize
+  IN      VOID    *SrcBuffer,
+  IN      UINT64  SrcSize,
+  IN      VOID    *DstBuffer,
+  IN OUT  UINT64  *DstSize
   )
 {
   EFI_STATUS  Status;

ShellPkg/Library/UefiShellDebug1CommandsLib/Compress.h
@@ -28,7 +28,6 @@
   @retval EFI_BUFFER_TOO_SMALL  The buffer was too small.  DstSize is required.
 **/
 EFI_STATUS
-EFIAPI
 Compress (
   IN      VOID    *SrcBuffer,
   IN      UINT64  SrcSize,