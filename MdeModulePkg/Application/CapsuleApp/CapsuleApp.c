Homerlv/edk2

commit dc01b59bc8a530ed01a1b0fcf6ee90df2af89860

MdeModulePkg CapsuleApp: Check Arg count for -P GET option
Also add help info for CapsuleApp -P GET option.

Cc: Michael D Kinney <michael.d.kinney@intel.com>
Cc: Jiewen Yao <jiewen.yao@intel.com>
Cc: Yonghong Zhu <yonghong.zhu@intel.com>
Contributed-under: TianoCore Contribution Agreement 1.1
Signed-off-by: Star Zeng <star.zeng@intel.com>
Reviewed-by: Jiewen Yao <jiewen.yao@intel.com>

MdeModulePkg/Application/CapsuleApp/CapsuleApp.c
@@ -740,7 +740,8 @@ PrintUsage (
   Print(L"       which is defined in UEFI specification.\n");
   Print(L"  -C:  Clear capsule report variable (EFI_CAPSULE_REPORT_GUID),\n");
   Print(L"       which is defined in UEFI specification.\n");
-  Print(L"  -P:  Dump UEFI FMP protocol info.\n");
+  Print(L"  -P:  Dump UEFI FMP protocol info, or get image with specified\n");
+  Print(L"       ImageTypeId and index to a file if 'GET' option is used.\n");
   Print(L"  -E:  Dump UEFI ESRT table info.\n");
   Print(L"  -G:  Convert a BMP file to be an UX capsule,\n");
   Print(L"       according to Windows Firmware Update document\n");
@@ -820,7 +821,15 @@ UefiMain (
       DumpFmpData();
     }
     if (Argc >= 3) {
-      if (StrCmp(Argv[2], L"GET") == 0) {
+      if (StrCmp(Argv[2], L"GET") != 0) {
+        Print(L"CapsuleApp: Unrecognized option(%s).\n", Argv[2]);
+        return EFI_UNSUPPORTED;
+      } else {
+        if (Argc != 7) {
+          Print(L"CapsuleApp: Incorrect parameter count.\n");
+          return EFI_UNSUPPORTED;
+        }
+
         EFI_GUID  ImageTypeId;
         UINTN     ImageIndex;
         //
@@ -832,9 +841,11 @@ UefiMain (
           return EFI_INVALID_PARAMETER;
         }
         ImageIndex = StrDecimalToUintn(Argv[4]);
-        if (StrCmp(Argv[5], L"-O") == 0) {
-          DumpFmpImage(&ImageTypeId, ImageIndex, Argv[6]);
+        if (StrCmp(Argv[5], L"-O") != 0) {
+          Print(L"CapsuleApp: NO output file name.\n");
+          return EFI_UNSUPPORTED;
         }
+        DumpFmpImage(&ImageTypeId, ImageIndex, Argv[6]);
       }
     }
     return EFI_SUCCESS;

commit cd17d175329c48da8312fdf3cf984a53fe3aeb67

MdeModulePkg CapsuleApp: Fix VS2012 build failure caused by dc01b59
The build failure is like below.
xxx\CapsuleApp.c(868) : error C2275: 'EFI_GUID' :
  illegal use of this type as an expression
        xxx/UefiBaseType.h(29) : see declaration of 'EFI_GUID'
xxx\CapsuleApp.c(868) : error C2146: syntax error :
  missing ';' before identifier 'ImageTypeId'
xxx\CapsuleApp.c(868) : error C2065: 'ImageTypeId' : undeclared identifier
xxx\CapsuleApp.c(869) : error C2275: 'UINTN' :
  illegal use of this type as an expression
        xxx\ProcessorBind.h(224) : see declaration of 'UINTN'
xxx\CapsuleApp.c(869) : error C2146: syntax error :
  missing ';' before identifier 'ImageIndex'
xxx\CapsuleApp.c(869) : error C2065: 'ImageIndex' : undeclared identifier

Cc: Jiewen Yao <jiewen.yao@intel.com>
Cc: Dandan Bi <dandan.bi@intel.com>
Contributed-under: TianoCore Contribution Agreement 1.1
Signed-off-by: Star Zeng <star.zeng@intel.com>
Reviewed-by: Jiewen Yao <jiewen.yao@intel.com>
Reviewed-by: Dandan Bi <dandan.bi@intel.com>

MdeModulePkg/Application/CapsuleApp/CapsuleApp.c
@@ -809,14 +809,16 @@ UefiMain (
   UINTN                         FileSize[MAX_CAPSULE_NUM];
   VOID                          *CapsuleBuffer[MAX_CAPSULE_NUM];
   EFI_CAPSULE_BLOCK_DESCRIPTOR  *BlockDescriptors;
-  EFI_CAPSULE_HEADER             *CapsuleHeaderArray[MAX_CAPSULE_NUM + 1];
-  UINT64                         MaxCapsuleSize;
-  EFI_RESET_TYPE                 ResetType;
-  BOOLEAN                        NeedReset;
-  BOOLEAN                        NoReset;
-  CHAR16                         *CapsuleName;
-  UINTN                          CapsuleNum;
-  UINTN                          Index;
+  EFI_CAPSULE_HEADER            *CapsuleHeaderArray[MAX_CAPSULE_NUM + 1];
+  UINT64                        MaxCapsuleSize;
+  EFI_RESET_TYPE                ResetType;
+  BOOLEAN                       NeedReset;
+  BOOLEAN                       NoReset;
+  CHAR16                        *CapsuleName;
+  UINTN                         CapsuleNum;
+  UINTN                         Index;
+  EFI_GUID                      ImageTypeId;
+  UINTN                         ImageIndex;
 
   Status = GetArg();
   if (EFI_ERROR(Status)) {
@@ -865,8 +867,6 @@ UefiMain (
           return EFI_UNSUPPORTED;
         }
 
-        EFI_GUID  ImageTypeId;
-        UINTN     ImageIndex;
         //
         // FMP->GetImage()
         //