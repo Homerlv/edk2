Homerlv/edk2

commit 25a616b8db5bdf1a876b1856accb50d0f31e8d9e

BaseTools/Ecc: Fix import issues
1. Complete the full path for import statement. Use "EccMain" to
replace "Ecc" for the absolute path support.
2. Fix some issues on configuration file.
3. Fix an issue of RaiseError not working in EdkLogger.

Contributed-under: TianoCore Contribution Agreement 1.0
Signed-off-by: Hess Chen <hesheng.chen@intel.com>
Reviewed-by: Liming Gao <liming.gao@intel.com>

BaseTools/Source/Python/Common/EdkLogger.py
@@ -1,7 +1,7 @@
 ## @file
 # This file implements the log mechanism for Python tools.
 #
-# Copyright (c) 2007 - 2015, Intel Corporation. All rights reserved.<BR>
+# Copyright (c) 2007 - 2018, Intel Corporation. All rights reserved.<BR>
 # This program and the accompanying materials
 # are licensed and made available under the terms and conditions of the BSD License
 # which accompanies this distribution.  The full text of the license may be found at
@@ -198,6 +198,7 @@ def error(ToolName, ErrorCode, Message=None, File=None, Line=None, ExtraData=Non
         LogText = _ErrorMessageTemplateWithoutFile % TemplateDict
 
     _ErrorLogger.log(ERROR, LogText)
+    RaiseError = IsRaiseError
     if RaiseError:
         raise FatalError(ErrorCode)
 

BaseTools/BinWrappers/PosixLike/Ecc
@@ -11,4 +11,4 @@ dir=$(dirname "$full_cmd")
 cmd=${full_cmd##*/}
 
 export PYTHONPATH="$dir/../../Source/Python${PYTHONPATH:+:"$PYTHONPATH"}"
-exec "${python_exe:-python}" -m $cmd.$cmd "$@"
+exec "${python_exe:-python}" -m $cmd.EccMain "$@"

BaseTools/BinWrappers/WindowsLike/Ecc.bat
@@ -1,4 +1,4 @@
 @setlocal
 @set ToolName=%~n0%
 @set PYTHONPATH=%PYTHONPATH%;%BASE_TOOLS_PATH%\Source\Python
-@%PYTHON_HOME%\python.exe -m %ToolName%.%ToolName% %*
+@%PYTHON_HOME%\python.exe -m %ToolName%.EccMain %*

BaseTools/Source/Python/Ecc/CParser.py
@@ -24,8 +24,8 @@ from antlr3.compat import set, frozenset
 #
 ##
 
-from . import CodeFragment
-from . import FileProfile
+from Ecc import CodeFragment
+from Ecc import FileProfile
 
 
 

BaseTools/Source/Python/Ecc/Check.py
@@ -15,10 +15,10 @@ import Common.LongFilePathOs as os
 import re
 from CommonDataClass.DataClass import *
 import Common.DataType as DT
-from .EccToolError import *
-from .MetaDataParser import ParseHeaderCommentSection
-from . import EccGlobalData
-from . import c
+from Ecc.EccToolError import *
+from Ecc.MetaDataParser import ParseHeaderCommentSection
+from Ecc import EccGlobalData
+from Ecc import c
 from Common.LongFilePathSupport import OpenLongFilePath as open
 from Common.MultipleWorkspace import MultipleWorkspace as mws
 

BaseTools/Source/Python/Ecc/CodeFragmentCollector.py
@@ -23,13 +23,13 @@ import Common.LongFilePathOs as os
 import sys
 
 import antlr3
-from .CLexer import CLexer
-from .CParser import CParser
+from Ecc.CLexer import CLexer
+from Ecc.CParser import CParser
 
-from . import FileProfile
-from .CodeFragment import Comment
-from .CodeFragment import PP_Directive
-from .ParserWarning import Warning
+from Ecc import FileProfile
+from Ecc.CodeFragment import Comment
+from Ecc.CodeFragment import PP_Directive
+from Ecc.ParserWarning import Warning
 
 
 ##define T_CHAR_SPACE                ' '

BaseTools/Source/Python/Ecc/Configuration.py
@@ -60,12 +60,14 @@ _ConfigFileToInternalTranslation = {
     "GeneralCheckIndentation":"GeneralCheckIndentation",
     "GeneralCheckIndentationWidth":"GeneralCheckIndentationWidth",
     "GeneralCheckLine":"GeneralCheckLine",
+    "GeneralCheckLineEnding":"GeneralCheckLineEnding",
     "GeneralCheckLineWidth":"GeneralCheckLineWidth",
     "GeneralCheckNoProgma":"GeneralCheckNoProgma",
     "GeneralCheckNoTab":"GeneralCheckNoTab",
     "GeneralCheckNo_Asm":"GeneralCheckNo_Asm",
     "GeneralCheckNonAcsii":"GeneralCheckNonAcsii",
     "GeneralCheckTabWidth":"GeneralCheckTabWidth",
+    "GeneralCheckTrailingWhiteSpaceLine":"GeneralCheckTrailingWhiteSpaceLine",
     "GeneralCheckUni":"GeneralCheckUni",
     "HeaderCheckAll":"HeaderCheckAll",
     "HeaderCheckCFileCommentLicenseFormat":"HeaderCheckCFileCommentLicenseFormat",

BaseTools/Source/Python/Ecc/Database.py
@@ -27,9 +27,9 @@ from Table.TableFunction import TableFunction
 from Table.TablePcd import TablePcd
 from Table.TableIdentifier import TableIdentifier
 from Table.TableReport import TableReport
-from .MetaFileWorkspace.MetaFileTable import ModuleTable
-from .MetaFileWorkspace.MetaFileTable import PackageTable
-from .MetaFileWorkspace.MetaFileTable import PlatformTable
+from Ecc.MetaFileWorkspace.MetaFileTable import ModuleTable
+from Ecc.MetaFileWorkspace.MetaFileTable import PackageTable
+from Ecc.MetaFileWorkspace.MetaFileTable import PlatformTable
 from Table.TableFdf import TableFdf
 
 ##

commit 74ea4bd003ad5b4b2a4ded282ad2ffe0e5ec4874

BaseTools: Fix the RaiseError variable issue caused by 25a616b8db5
The bug is that it cause the RaiseError always be set to TRUE even we
call the function with FALSE parameter.

Cc: Hess Chen <hesheng.chen@intel.com>
Cc: Liming Gao <liming.gao@intel.com>
Contributed-under: TianoCore Contribution Agreement 1.1
Signed-off-by: Yonghong Zhu <yonghong.zhu@intel.com>
Reviewed-by: Hess Chen <hesheng.chen@intel.com>

BaseTools/Source/Python/Common/EdkLogger.py
@@ -198,8 +198,8 @@ def error(ToolName, ErrorCode, Message=None, File=None, Line=None, ExtraData=Non
         LogText = _ErrorMessageTemplateWithoutFile % TemplateDict
 
     _ErrorLogger.log(ERROR, LogText)
-    RaiseError = IsRaiseError
-    if RaiseError:
+
+    if RaiseError and IsRaiseError:
         raise FatalError(ErrorCode)
 
 # Log information which should be always put out