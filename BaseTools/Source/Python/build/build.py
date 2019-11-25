Homerlv/edk2

commit cd8db1f282f1df4747b5646ff1fd0f9a9ab3177d

BaseTools: Move Build Cache related function out of CreateAsBuiltInf
BZ: https://bugzilla.tianocore.org/show_bug.cgi?id=1932
There are two functions in current CreateAsBuiltInf, Copy Binary files
to build cache folder and create asbuild inf file.

This patch is to separate UpdateBuildCache and CreateAsBuiltInf into
two functions.

Signed-off-by: Bob Feng <bob.c.feng@intel.com>
Cc: Steven Shi <steven.shi@intel.com>
Cc: Liming Gao <liming.gao@intel.com>
Cc: Christian Rodriguez <christian.rodriguez@intel.com>
Reviewed-by: Steven Shi <steven.shi@intel.com>
Reviewed-by: Jaben Carsey <jaben.carsey@intel.com>

BaseTools/Source/Python/build/build.py
@@ -1250,6 +1250,9 @@ class Build():
             BuildCommand = BuildCommand + [Target]
             LaunchCommand(BuildCommand, AutoGenObject.MakeFileDir)
             self.CreateAsBuiltInf()
+            if GlobalData.gBinCacheDest:
+                self.UpdateBuildCache()
+            self.BuildModules = []
             return True
 
         # build library
@@ -1268,6 +1271,9 @@ class Build():
                 NewBuildCommand = BuildCommand + ['-f', os.path.normpath(os.path.join(Mod, makefile)), 'pbuild']
                 LaunchCommand(NewBuildCommand, AutoGenObject.MakeFileDir)
             self.CreateAsBuiltInf()
+            if GlobalData.gBinCacheDest:
+                self.UpdateBuildCache()
+            self.BuildModules = []
             return True
 
         # cleanlib
@@ -1361,6 +1367,9 @@ class Build():
                 BuildCommand = BuildCommand + [Target]
             AutoGenObject.BuildTime = LaunchCommand(BuildCommand, AutoGenObject.MakeFileDir)
             self.CreateAsBuiltInf()
+            if GlobalData.gBinCacheDest:
+                self.UpdateBuildCache()
+            self.BuildModules = []
             return True
 
         # genfds
@@ -1874,6 +1883,9 @@ class Build():
                 ExitFlag.set()
                 BuildTask.WaitForComplete()
                 self.CreateAsBuiltInf()
+                if GlobalData.gBinCacheDest:
+                    self.UpdateBuildCache()
+                self.BuildModules = []
                 self.MakeTime += int(round((time.time() - MakeContiue)))
                 if BuildTask.HasError():
                     self.invalidateHash()
@@ -2074,6 +2086,9 @@ class Build():
                 ExitFlag.set()
                 BuildTask.WaitForComplete()
                 self.CreateAsBuiltInf()
+                if GlobalData.gBinCacheDest:
+                    self.UpdateBuildCache()
+                self.BuildModules = []
                 self.MakeTime += int(round((time.time() - MakeContiue)))
                 #
                 # Check for build error, and raise exception if one
@@ -2213,24 +2228,25 @@ class Build():
             RemoveDirectory(os.path.dirname(GlobalData.gDatabasePath), True)
 
     def CreateAsBuiltInf(self):
+        for Module in self.BuildModules:
+            Module.CreateAsBuiltInf()
+
+    def UpdateBuildCache(self):
         all_lib_set = set()
         all_mod_set = set()
         for Module in self.BuildModules:
-            Module.CreateAsBuiltInf()
+            Module.CopyModuleToCache()
             all_mod_set.add(Module)
         for Module in self.HashSkipModules:
-            if  GlobalData.gBinCacheDest:
-                Module.CopyModuleToCache()
+            Module.CopyModuleToCache()
             all_mod_set.add(Module)
         for Module in all_mod_set:
             for lib in Module.LibraryAutoGenList:
                 all_lib_set.add(lib)
         for lib in all_lib_set:
-            if  GlobalData.gBinCacheDest:
-                lib.CopyModuleToCache()
+            lib.CopyModuleToCache()
         all_lib_set.clear()
         all_mod_set.clear()
-        self.BuildModules = []
         self.HashSkipModules = []
     ## Do some clean-up works when error occurred
     def Relinquish(self):

BaseTools/Source/Python/AutoGen/AutoGen.py
@@ -3579,19 +3579,37 @@ class ModuleAutoGen(AutoGen):
         fInputfile.close ()
         return OutputName
 
+    @cached_property
+    def OutputFile(self):
+        retVal = set()
+        OutputDir = self.OutputDir.replace('\\', '/').strip('/')
+        DebugDir = self.DebugDir.replace('\\', '/').strip('/')
+        for Item in self.CodaTargetList:
+            File = Item.Target.Path.replace('\\', '/').strip('/').replace(DebugDir, '').replace(OutputDir, '').strip('/')
+            retVal.add(File)
+        if self.DepexGenerated:
+            retVal.add(self.Name + '.depex')
+
+        Bin = self._GenOffsetBin()
+        if Bin:
+            retVal.add(Bin)
+
+        for Root, Dirs, Files in os.walk(OutputDir):
+            for File in Files:
+                if File.lower().endswith('.pdb'):
+                    retVal.add(File)
+
+        return retVal
+
     ## Create AsBuilt INF file the module
     #
     def CreateAsBuiltInf(self):
-        self.OutputFile = set()
 
         if self.IsAsBuiltInfCreated:
             return
 
         # Skip INF file generation for libraries
         if self.IsLibrary:
-            # Only store the library cache if needed
-            if GlobalData.gBinCacheDest:
-                self.CopyModuleToCache()
             return
 
         # Skip the following code for modules with no source files
@@ -3712,7 +3730,6 @@ class ModuleAutoGen(AutoGen):
         DebugDir = self.DebugDir.replace('\\', '/').strip('/')
         for Item in self.CodaTargetList:
             File = Item.Target.Path.replace('\\', '/').strip('/').replace(DebugDir, '').replace(OutputDir, '').strip('/')
-            self.OutputFile.add(File)
             if os.path.isabs(File):
                 File = File.replace('\\', '/').strip('/').replace(OutputDir, '').strip('/')
             if Item.Target.Ext.lower() == '.aml':
@@ -3728,7 +3745,6 @@ class ModuleAutoGen(AutoGen):
             if os.path.exists(DepexFile):
                 self.DepexGenerated = True
         if self.DepexGenerated:
-            self.OutputFile.add(self.Name + '.depex')
             if self.ModuleType in [SUP_MODULE_PEIM]:
                 AsBuiltInfDict['binary_item'].append('PEI_DEPEX|' + self.Name + '.depex')
             elif self.ModuleType in [SUP_MODULE_DXE_DRIVER, SUP_MODULE_DXE_RUNTIME_DRIVER, SUP_MODULE_DXE_SAL_DRIVER, SUP_MODULE_UEFI_DRIVER]:
@@ -3739,13 +3755,11 @@ class ModuleAutoGen(AutoGen):
         Bin = self._GenOffsetBin()
         if Bin:
             AsBuiltInfDict['binary_item'].append('BIN|%s' % Bin)
-            self.OutputFile.add(Bin)
 
         for Root, Dirs, Files in os.walk(OutputDir):
             for File in Files:
                 if File.lower().endswith('.pdb'):
                     AsBuiltInfDict['binary_item'].append('DISPOSABLE|' + File)
-                    self.OutputFile.add(File)
         HeaderComments = self.Module.HeaderComments
         StartPos = 0
         for Index in range(len(HeaderComments)):
@@ -3914,8 +3928,6 @@ class ModuleAutoGen(AutoGen):
         SaveFileOnChange(os.path.join(self.OutputDir, self.Name + '.inf'), str(AsBuiltInf), False)
 
         self.IsAsBuiltInfCreated = True
-        if GlobalData.gBinCacheDest:
-            self.CopyModuleToCache()
 
     def CopyModuleToCache(self):
         FileDir = path.join(GlobalData.gBinCacheDest, self.PlatformInfo.OutputDir, self.BuildTarget + "_" + self.ToolChain, self.Arch, self.SourceDir, self.MetaFile.BaseName)
@@ -3923,30 +3935,24 @@ class ModuleAutoGen(AutoGen):
         HashFile = path.join(self.BuildDir, self.Name + '.hash')
         if os.path.exists(HashFile):
             CopyFileOnChange(HashFile, FileDir)
-        if not self.IsLibrary:
-            ModuleFile = path.join(self.OutputDir, self.Name + '.inf')
-            if os.path.exists(ModuleFile):
-                CopyFileOnChange(ModuleFile, FileDir)
-        else:
-            OutputDir = self.OutputDir.replace('\\', '/').strip('/')
-            DebugDir = self.DebugDir.replace('\\', '/').strip('/')
-            for Item in self.CodaTargetList:
-                File = Item.Target.Path.replace('\\', '/').strip('/').replace(DebugDir, '').replace(OutputDir, '').strip('/')
-                self.OutputFile.add(File)
+        ModuleFile = path.join(self.OutputDir, self.Name + '.inf')
+        if os.path.exists(ModuleFile):
+            CopyFileOnChange(ModuleFile, FileDir)
+
         if not self.OutputFile:
             Ma = self.BuildDatabase[self.MetaFile, self.Arch, self.BuildTarget, self.ToolChain]
             self.OutputFile = Ma.Binaries
-        if self.OutputFile:
-            for File in self.OutputFile:
-                File = str(File)
-                if not os.path.isabs(File):
-                    File = os.path.join(self.OutputDir, File)
-                if os.path.exists(File):
-                    sub_dir = os.path.relpath(File, self.OutputDir)
-                    destination_file = os.path.join(FileDir, sub_dir)
-                    destination_dir = os.path.dirname(destination_file)
-                    CreateDirectory(destination_dir)
-                    CopyFileOnChange(File, destination_dir)
+
+        for File in self.OutputFile:
+            File = str(File)
+            if not os.path.isabs(File):
+                File = os.path.join(self.OutputDir, File)
+            if os.path.exists(File):
+                sub_dir = os.path.relpath(File, self.OutputDir)
+                destination_file = os.path.join(FileDir, sub_dir)
+                destination_dir = os.path.dirname(destination_file)
+                CreateDirectory(destination_dir)
+                CopyFileOnChange(File, destination_dir)
 
     def AttemptModuleCacheCopy(self):
         # If library or Module is binary do not skip by hash

commit 8e7b5a8f16f6a25fb35380d992a54c24f9580498

BaseTools:fix regression issue for platform .map file
BZ:https://bugzilla.tianocore.org/show_bug.cgi?id=2363

This patch is to fix a build tool regression issue which was introduced
by commit cd8db1f282.This issue caused map file lost the line of IMAGE=***.
For example,in Ovmf.map, there is no line of (IMAGE=<path to efi> ) under
each of modules item.

The path to the efi file generated by each module is written on this line
The purpose of this line is add the debug image full path.
there is no information about the module in the map file other than FVName,
it allows us to quickly know which module this part corresponds to.

In commit cd8db1f282,add a line ("self.BuildModules = []") in function,
but it's used to calculate the variable ModuleList in the following code.

Signed-off-by: Zhiju.Fan <zhijux.fan@intel.com>

Cc: Liming Gao <liming.gao@intel.com>
Cc: Bob Feng <bob.c.feng@intel.com>
Reviewed-by: Bob Feng <bob.c.feng@intel.com>

BaseTools/Source/Python/build/build.py
@@ -2267,6 +2267,10 @@ class Build():
                 self.CreateAsBuiltInf()
                 if GlobalData.gBinCacheDest:
                     self.UpdateBuildCache()
+                #
+                # Get Module List
+                #
+                ModuleList = {ma.Guid.upper(): ma for ma in self.BuildModules}
                 self.BuildModules = []
                 self.MakeTime += int(round((time.time() - MakeContiue)))
                 #
@@ -2285,10 +2289,6 @@ class Build():
                         #
                         if (Arch == 'IA32' or Arch == 'ARM') and self.LoadFixAddress != 0xFFFFFFFFFFFFFFFF and self.LoadFixAddress >= 0x100000000:
                             EdkLogger.error("build", PARAMETER_INVALID, "FIX_LOAD_TOP_MEMORY_ADDRESS can't be set to larger than or equal to 4G for the platorm with IA32 or ARM arch modules")
-                    #
-                    # Get Module List
-                    #
-                    ModuleList = {ma.Guid.upper():ma for ma in self.BuildModules}
 
                     #
                     # Rebase module to the preferred memory address before GenFds