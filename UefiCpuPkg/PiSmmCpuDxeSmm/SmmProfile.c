Homerlv/edk2

commit 77e3d57f4959bcb1d25476739f7eee4d370b9fea

UefiCpuPkg/PiSmmCpuDxeSmm: Fix system hang when SmmProfile enable
MMIO ranges within the mProtectionMemRange array may exceed 4G
and should be configured as 'Present & NX'. However, the initial
attribute for these MMIO addresses in the page table is
'non-present'. Other attributes should not be set or updated for
a non-present range if the present bit mask is zero, as this could
result in an error during the InitPaging for the page table update
process.

This patch is to resolve the error to make sure MMIO page table
can be configured correctly.

Signed-off-by: Jiaxin Wu <jiaxin.wu@intel.com>
Cc: Ray Ni <ray.ni@intel.com>
Cc: Rahul Kumar <rahul1.kumar@intel.com>
Cc: Gerd Hoffmann <kraxel@redhat.com>

UefiCpuPkg/PiSmmCpuDxeSmm/SmmProfile.c
@@ -1,7 +1,7 @@
 /** @file
 Enable SMM profile.
 
-Copyright (c) 2012 - 2023, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2012 - 2024, Intel Corporation. All rights reserved.<BR>
 Copyright (c) 2017 - 2020, AMD Incorporated. All rights reserved.<BR>
 
 SPDX-License-Identifier: BSD-2-Clause-Patent
@@ -594,6 +594,7 @@ InitPaging (
   UINT64         Limit;
   UINT64         PreviousAddress;
   UINT64         MemoryAttrMask;
+  BOOLEAN        IsSet;
   BOOLEAN        WriteProtect;
   BOOLEAN        CetEnabled;
 
@@ -616,19 +617,38 @@ InitPaging (
   DEBUG ((DEBUG_INFO, "Patch page table start ...\n"));
   if (FeaturePcdGet (PcdCpuSmmProfileEnable)) {
     for (Index = 0; Index < mProtectionMemRangeCount; Index++) {
-      MemoryAttrMask = 0;
-      if (mProtectionMemRange[Index].Nx == TRUE) {
+      Base   = mProtectionMemRange[Index].Range.Base;
+      Length = mProtectionMemRange[Index].Range.Top - Base;
+
+      MemoryAttrMask = EFI_MEMORY_RP;
+      if (!mProtectionMemRange[Index].Present) {
+        //
+        // Config the EFI_MEMORY_RP attribute to make it non-present.
+        //
+        IsSet = TRUE;
+      } else {
+        //
+        // Clear the EFI_MEMORY_RP attribute to make it present.
+        //
+        IsSet = FALSE;
+
+        //
+        // Config the range as writable and executable when mapping a range as present.
+        //
+        MemoryAttrMask |= EFI_MEMORY_RO;
         MemoryAttrMask |= EFI_MEMORY_XP;
       }
 
-      if (mProtectionMemRange[Index].Present == FALSE) {
-        MemoryAttrMask = EFI_MEMORY_RP;
-      }
+      Status = ConvertMemoryPageAttributes (PageTable, mPagingMode, Base, Length, MemoryAttrMask, IsSet, NULL);
+      ASSERT_RETURN_ERROR (Status);
 
-      Base   = mProtectionMemRange[Index].Range.Base;
-      Length = mProtectionMemRange[Index].Range.Top - Base;
-      if (MemoryAttrMask != 0) {
-        Status = ConvertMemoryPageAttributes (PageTable, mPagingMode, Base, Length, MemoryAttrMask, TRUE, NULL);
+      if (mProtectionMemRange[Index].Present && mProtectionMemRange[Index].Nx) {
+        //
+        // Since EFI_MEMORY_XP has already been cleared above, only handle the case to disable execution.
+        // Config the EFI_MEMORY_XP attribute to disable execution.
+        //
+        MemoryAttrMask = EFI_MEMORY_XP;
+        Status         = ConvertMemoryPageAttributes (PageTable, mPagingMode, Base, Length, MemoryAttrMask, TRUE, NULL);
         ASSERT_RETURN_ERROR (Status);
       }
 

commit c26e5808480381cd5e5bc442844d40338d743bad

UefiCpuPkg: Revert "UefiCpuPkg/PiSmmCpuDxeSmm: Fix system..."
This reverts commit 77e3d57f49 "UefiCpuPkg/PiSmmCpuDxeSmm:
Fix system hang when SmmProfile enable".

The commit 77e3d57f49 was added to modify the code logic in
InitPaging() to fix a code assert issue. Previously, the root
cause of this issue is that we try to only set NX attribute
for not-present MMIO range above 4G when SMM profile feature
is enabled, which is not allowed by CpuPageTableLib.

But after we always create full mapping initial SMM page
table in the next commit, this code assert issue won't happen
anymore since MMIO range above 4g will also be present in SMM
page table before InitPaging().

Meanwhile another issue was introduced by commit 77e3d57f49:
In the entrypoint of PiSmmCpuDxe driver, we will set some
pages in stack range as not-present in SMM page table if
PcdCpuSmmStackGuard or PcdControlFlowEnforcementPropertyMask
is TRUE. But in commit 77e3d57f49, all SMRAM range are set
to present in InitPaging() if SMM profile is enabled. Then
the stack guard and shadow stack features do not work anymore.

So let's revert the commit "UefiCpuPkg/PiSmmCpuDxeSmm: Fix
system hang when SmmProfile enable"

Signed-off-by: Dun Tan <dun.tan@intel.com>

UefiCpuPkg/PiSmmCpuDxeSmm/SmmProfile.c
@@ -1,7 +1,7 @@
 /** @file
 Enable SMM profile.
 
-Copyright (c) 2012 - 2024, Intel Corporation. All rights reserved.<BR>
+Copyright (c) 2012 - 2023, Intel Corporation. All rights reserved.<BR>
 Copyright (c) 2017 - 2020, AMD Incorporated. All rights reserved.<BR>
 
 SPDX-License-Identifier: BSD-2-Clause-Patent
@@ -610,7 +610,6 @@ InitPaging (
   UINT64         Limit;
   UINT64         PreviousAddress;
   UINT64         MemoryAttrMask;
-  BOOLEAN        IsSet;
   BOOLEAN        WriteProtect;
   BOOLEAN        CetEnabled;
 
@@ -633,38 +632,19 @@ InitPaging (
   DEBUG ((DEBUG_INFO, "Patch page table start ...\n"));
   if (FeaturePcdGet (PcdCpuSmmProfileEnable)) {
     for (Index = 0; Index < mProtectionMemRangeCount; Index++) {
-      Base   = mProtectionMemRange[Index].Range.Base;
-      Length = mProtectionMemRange[Index].Range.Top - Base;
-
-      MemoryAttrMask = EFI_MEMORY_RP;
-      if (!mProtectionMemRange[Index].Present) {
-        //
-        // Config the EFI_MEMORY_RP attribute to make it non-present.
-        //
-        IsSet = TRUE;
-      } else {
-        //
-        // Clear the EFI_MEMORY_RP attribute to make it present.
-        //
-        IsSet = FALSE;
-
-        //
-        // Config the range as writable and executable when mapping a range as present.
-        //
-        MemoryAttrMask |= EFI_MEMORY_RO;
+      MemoryAttrMask = 0;
+      if (mProtectionMemRange[Index].Nx == TRUE) {
         MemoryAttrMask |= EFI_MEMORY_XP;
       }
 
-      Status = ConvertMemoryPageAttributes (PageTable, mPagingMode, Base, Length, MemoryAttrMask, IsSet, NULL);
-      ASSERT_RETURN_ERROR (Status);
+      if (mProtectionMemRange[Index].Present == FALSE) {
+        MemoryAttrMask = EFI_MEMORY_RP;
+      }
 
-      if (mProtectionMemRange[Index].Present && mProtectionMemRange[Index].Nx) {
-        //
-        // Since EFI_MEMORY_XP has already been cleared above, only handle the case to disable execution.
-        // Config the EFI_MEMORY_XP attribute to disable execution.
-        //
-        MemoryAttrMask = EFI_MEMORY_XP;
-        Status         = ConvertMemoryPageAttributes (PageTable, mPagingMode, Base, Length, MemoryAttrMask, TRUE, NULL);
+      Base   = mProtectionMemRange[Index].Range.Base;
+      Length = mProtectionMemRange[Index].Range.Top - Base;
+      if (MemoryAttrMask != 0) {
+        Status = ConvertMemoryPageAttributes (PageTable, mPagingMode, Base, Length, MemoryAttrMask, TRUE, NULL);
         ASSERT_RETURN_ERROR (Status);
       }
 